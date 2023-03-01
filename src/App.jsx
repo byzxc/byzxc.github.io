f33eca873c6709d9b12c04b062938cb2ecf11282
byzxc <darrenlinby@hotmail.com>
2023-03-02 00:40:10 +0800

    Added Card-Container, Card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,80 @@
-import "./App.css";
 import React from "react";
 import {
-  Container,
-  Navbar,
-  /*Nav , NavDropdown ,*/
-  NavbarBrand,
+    Container,
+    Navbar,
+    NavbarBrand,
 } from "react-bootstrap";
-import { Route, Routes, NavLink, BrowserRouter } from "react-router-dom";
-import Home from "./Contents/Home";
-import AboutMe from "./Contents/AboutMe";
-import Projects from "./Contents/Projects";
-import ContactMe from "./Contents/ContactMe";
+import {NavLink, BrowserRouter} from "react-router-dom";
+import logo from "./Contents/logo/me.jpg";
+import classNames from 'classnames/bind';
+import styles from './App.scss';
+
+
+const cx = classNames.bind(styles);
 
 class Main extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        {/* NavBar */}
-        <div>  
-          <Navbar id="navbar" fixed="top-left" bg="dark" variant="dark">
-            <Container>
-              <ul className="header">
-                {/*Brand works like your Logo, so when user press on it, it should return back to homepage*/}
-                <li>
-                  <NavbarBrand href="/">Home</NavbarBrand>
-                </li>
-                <li>
-                  <NavLink to="/Contents/AboutMe">About Me</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/Contents/Projects">Projects</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/Contents/ContactMe">Contact Me</NavLink>
-                </li>
-              </ul>
-            </Container>
-          </Navbar>
-        </div>
-        
-        <div className="content">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/Contents/AboutMe" element={<AboutMe />} />
-            <Route path="/Contents/Projects" element={<Projects />} />
-            <Route path="/Contents/ContactMe" element={<ContactMe />} />
-          </Routes>
-        </div>
-        
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                {/* NavBar */}
+                <div>
+                    <Navbar id="navbar" fixed="top-left" bg="dark" variant="dark">
+                        <Container>
+                            <ul className="header">
+                                {/*Brand works like your Logo, so when user press on it, it should return back to homepage*/}
+                                <li>
+                                    <NavbarBrand href="/">Home</NavbarBrand>
+                                </li>
+                                <li>
+                                    <NavLink to="/Contents/AboutMe">About Me</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/Contents/Projects">Projects</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/Contents/ContactMe">Contact Me</NavLink>
+                                </li>
+                            </ul>
+                        </Container>
+                    </Navbar>
+                </div>
+
+                <div className="content">
+                    <h1>Darren Lin</h1>
+                    <div className={cx("my-card__container")}>
+                        <div className={cx("my-card")}>
+                            <img className={cx("my-card__image")} alt="me" src={logo}/>
+                            <p>
+                                Seeking a promising position, in the information & technology industry, that offers good
+                                opportunities for learning experiences, project developments, professional growth and
+                                career
+                                advancement.
+                            </p>
+                            <div className={cx("my-card__buttons")}>
+                                <a href="https://drive.google.com/file/d/1WDGgl2OGQ_cDxmD6amX-IUUqJI9PTa4L/view?usp=share_link">Resume</a>
+                                <a href="https://www.linkedin.com/in/darrenlinby/">LinkedIn</a>
+                            </div>
+                        </div>
+                        <div className={cx("my-card1")}>
+                            <img className="my-card__image" alt="me" src={logo}/>
+                            <p>
+                                Seeking a promising position, in the information & technology industry, that offers good
+                                opportunities for learning experiences, project developments, professional growth and
+                                career
+                                advancement.
+                            </p>
+                            <div className={cx("my-card__buttons")}>
+                                <a href="https://drive.google.com/file/d/1WDGgl2OGQ_cDxmD6amX-IUUqJI9PTa4L/view?usp=share_link">Resume</a>
+                                <a href="https://www.linkedin.com/in/darrenlinby/">LinkedIn</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Footer */}
+            </BrowserRouter>
+        );
+    }
 }
 
 export default Main;
